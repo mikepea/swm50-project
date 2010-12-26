d272b5273e4985c0f07d7e1ac197c5aea20ff63e
Mike Pountney <mike@Mike-Pountneys-MacBook.local>
2010-12-26 18:37:18 +0000

    init rails app for rateyourroast

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rateyourroast::Application.config.secret_token = 'b6caa58173bc8b62d9f6f6e04358c44c44a248dd97cc19e9c7adc43664345db8ab1ed9712b025759ea3d85670ef6dccce9e478ccd915efed6fe38348035304e9'
