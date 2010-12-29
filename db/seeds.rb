7b94a7576f9181a10c9d834103c0b711d60378a8
Mike Pountney <Mike.Pountney@gmail.com>
2010-12-29 08:55:44 +0000

    db seed, tests run happily

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+Dish.delete_all 
+Location.delete_all 
+City.delete_all 
+
+City.create(:name => 'Brighton')
+Location.create(:name => 'The Hampton', :postcode => 'BN1 3XX')
+Location.create(:name => 'The Windmill', :postcode => 'BN1 3XY')
+Dish.create(:name => 'Lamb Roast',
+            :description => %{<p>
+<p>Well cooked lamb, with: </p>
+<ul> 
+<li>3 Roast Potatoes</li> 
+<li>Cauliflower Cheese</li> 
+<li>Nice veg mix</li> 
+</ul> 
+<p>Well presented with a selection of condiments</p>
+})
