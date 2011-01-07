1210465f203f6d7e927a6cbdcc359e0ee92e37ef
Mike Pountney <Mike.Pountney@gmail.com>
2011-01-07 02:31:50 +0000

    navigation kind of working now, but theres bugs with adding dishes and reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,22 +1,8 @@
 class Review < ActiveRecord::Base
 
     validates :review_body, :presence => true
+    validates :dish_id, :presence => true, :numericality => { :only_integer => true }
+
     belongs_to :dish
-
-    def current_description 
-        ld = LocationDescription.find_last_by_location_id(self.id)
-        puts "current_description: #{self.id}\n"
-        if ld 
-            ld.description
-        else
-            ""
-        end
-    end
-
-    def current_description=(desc)
-        ld = LocationDescription.new(:location_id => self.id, :description => desc)
-        ld.save
-        desc
-    end
 
 end
